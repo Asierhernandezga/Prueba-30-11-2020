9e804052c1d9d5be340de1b0d6290c47b5e7cfa0
Asierhernandezga <75242363+Asierhernandezga@users.noreply.github.com>
2020-11-30 10:53:08 +0100

    Creado el metodo funciones matematicas

--- MiPrimerDia/src/main/java/MiPrimerDia/FuncionesMatematicas.java
+++ MiPrimerDia/src/main/java/MiPrimerDia/FuncionesMatematicas.java
@@ -2,9 +2,8 @@ package MiPrimerDia;
 
 public class FuncionesMatematicas {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public FuncionesMatematicas() {
 
 	}
-
+	
 }
